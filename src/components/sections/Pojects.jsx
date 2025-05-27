3ae197549f25da7e137f93a59783467bb6d80ce7
Jose Arnedo <arnedo1@msn.com>
2025-05-27 18:08:53 +0200

    added reveal onsctoll per project

--- src/components/sections/Pojects.jsx
+++ src/components/sections/Pojects.jsx
@@ -93,16 +93,16 @@ export const Projects = () => {
                         </div>
                     </RevealOnScroll>
                     <RevealOnScroll>
-                        <div className="bg-gray-800/50 transition hover:scale-101  hover:shadow-[0_0_15px_rgba(59,130,246,0.4)] rounded-lg p-6 backdrop-blur-sm border border-gray-700">
-                            <h3 className="text-xl text-center font-bold mb-2">My Shopping List</h3>
-                            <img src={ imgSoppinglistMob } alt="My ShoppingList app preview" className="w-2/4 mx-auto h-auto mb-4" />
-                            <h3 className="text-xl font-bold mb-2">Small and effective shoppinglist</h3>
+                    <div className="bg-gray-800/50  hover:shadow-[0_0_15px_rgba(59,130,246,0.4)] transition hover:scale-101 rounded-lg p-6 backdrop-blur-sm border border-gray-700">
+                            <h3 className="text-xl font-bold mb-2">SuperCook Recipe Finder</h3>
+                            <img src={ imgMealFinder } alt="SuperCook Recipe Finder" className="w-full h-auto mb-4" />
+                            <h3 className="text-xl font-bold mb-2">Responsive recipes finder application</h3>
                             <p className="text-gray-300 mb-4">
-                                This is a small but handy shopping list for day to day use. You can add, remove, edit ans check when completed.
+                                SuperCook is a sleek React app that lets you discover, search, and save recipes with an intuitive interface, complete with ingredients, cooking instructions, and easy favorites management.
                             </p>
                             <div className="flex justify-around items-center">
                                 <div className="flex flex-wrap max-w-[300px] gap-2">
-                                    { [ 'HTML', 'CSS', 'JavaScript' ].map( ( tech, key ) => (
+                                    { [ 'HTML', 'CSS', 'JavaScript', 'React', 'Tailwind' ].map( ( tech, key ) => (
                                         <span
                                             key={ key }
                                             className="bg-blue-500/10 text-blue-500 h-8 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition inline-flex items-center"
@@ -111,11 +111,13 @@ export const Projects = () => {
                                         </span>
                                     ) ) }
                                 </div>
-                                <div className="flex flex-wrap gap-2 items-center ">
-                                    <a href="https://arnedoschildersbedrijf.nl/" className="bg-blue-500 text-white h-8 px-4 rounded font-medium transition inline-flex items-center relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]">Website</a>
+                                <div className="flex items-center flex-wrap gap-2">
+                                    <a href="https://arnedo1.github.io/Meal-Finder/" className="bg-blue-500 text-white h-8 px-4 rounded font-medium transition inline-flex items-center relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]">Website</a>
+
                                 </div>
                             </div>
                         </div>
+                        
                     </RevealOnScroll>
                     <RevealOnScroll>
                         <div className="bg-gray-800/50 transition hover:scale-101  hover:shadow-[0_0_15px_rgba(59,130,246,0.4)] rounded-lg p-6 backdrop-blur-sm border border-gray-700">
@@ -143,16 +145,16 @@ export const Projects = () => {
                         </div>
                     </RevealOnScroll>
                     <RevealOnScroll>
-                        <div className="bg-gray-800/50  hover:shadow-[0_0_15px_rgba(59,130,246,0.4)] transition hover:scale-101 rounded-lg p-6 backdrop-blur-sm border border-gray-700">
-                            <h3 className="text-xl font-bold mb-2">SuperCook Recipe Finder</h3>
-                            <img src={ imgMealFinder } alt="SuperCook Recipe Finder" className="w-full h-auto mb-4" />
-                            <h3 className="text-xl font-bold mb-2">Responsive recipes finder application</h3>
+                    <div className="bg-gray-800/50 transition hover:scale-101  hover:shadow-[0_0_15px_rgba(59,130,246,0.4)] rounded-lg p-6 backdrop-blur-sm border border-gray-700">
+                            <h3 className="text-xl text-center font-bold mb-2">My Shopping List</h3>
+                            <img src={ imgSoppinglistMob } alt="My ShoppingList app preview" className="w-2/4 mx-auto h-auto mb-4" />
+                            <h3 className="text-xl font-bold mb-2">Small and effective shoppinglist</h3>
                             <p className="text-gray-300 mb-4">
-                                SuperCook is a sleek React app that lets you discover, search, and save recipes with an intuitive interface, complete with ingredients, cooking instructions, and easy favorites management.
+                                This is a small but handy shopping list for day to day use. You can add, remove, edit ans check when completed.
                             </p>
                             <div className="flex justify-around items-center">
                                 <div className="flex flex-wrap max-w-[300px] gap-2">
-                                    { [ 'HTML', 'CSS', 'JavaScript', 'React', 'Tailwind' ].map( ( tech, key ) => (
+                                    { [ 'HTML', 'CSS', 'JavaScript' ].map( ( tech, key ) => (
                                         <span
                                             key={ key }
                                             className="bg-blue-500/10 text-blue-500 h-8 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition inline-flex items-center"
@@ -161,9 +163,8 @@ export const Projects = () => {
                                         </span>
                                     ) ) }
                                 </div>
-                                <div className="flex items-center flex-wrap gap-2">
-                                    <a href="https://arnedo1.github.io/Meal-Finder/" className="bg-blue-500 text-white h-8 px-4 rounded font-medium transition inline-flex items-center relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]">Website</a>
-
+                                <div className="flex flex-wrap gap-2 items-center ">
+                                    <a href="https://arnedoschildersbedrijf.nl/" className="bg-blue-500 text-white h-8 px-4 rounded font-medium transition inline-flex items-center relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]">Website</a>
                                 </div>
                             </div>
                         </div>
